99f5f9adb9bb7c9ac0dd52306e8b84e667959880
Nicolás Alonso Suárez Rodríguez <nasuarezro@unal.edu.co>
2025-06-17 10:48:22 -0500

    update README.md with hotel management system details and features

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -31,6 +31,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+
+        Commands\SetupCommand::prohibit($this->app->isProduction());
+        Commands\InstallCommand::prohibit($this->app->isProduction());
+        Commands\GenerateCommand::prohibit($this->app->isProduction());
+        Commands\PublishCommand::prohibit($this->app->isProduction());
     }
 }
